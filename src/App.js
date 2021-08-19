fc85d566eaf03eaa294dbb8d62f3fb77b4357dcc
skydrol <keineleben@gmail.com>
2021-08-19 19:42:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, createRef } from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: '',
+      inputValue: ''
+    };
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  mainFunction = () => {
+    let result = this.sandbox();
+    this.setState({ output: result });
+    this.textInput = createRef();
+
+  }
+
+  sandbox = () => {
+
+    let input = this.state.inputValue;
+
+    // Your code here
+    // example: 
+    // Sum 10 to the input number:     
+    
+    let result = parseFloat(input) + 10;
+
+    return result ;
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Sandbox</h1>
+        <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} />
+        <br /><br />
+        <button onClick={this.mainFunction}>Execute</button>
+        <br /><br />
+        <p id="output" ref="output">{this.state.output}</p>
+      </div>
+    );
+  }
 }
 
+
+
 export default App;
+
